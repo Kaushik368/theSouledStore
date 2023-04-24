de2b4594c024e1ab04c34836ad20968c1c63ec7d
Kaushik <kaubis368@gmail.com>
2023-04-24 14:00:53 +0530

    report

--- Cypress Project/cucumber-html-report.js
+++ Cypress Project/cucumber-html-report.js
@@ -1,16 +1,26 @@
 const report = require("multiple-cucumber-html-reporter");
 report.generate({
-jsonDir: "cypress/cucumber-json",  // ** Path of .json file **//
-reportPath: "./reports/cucumber-htmlreport.html",
-metadata: {
-browser: {
-name: "chrome",
-version: "81",
-},
-device: "Local test machine",
-platform: {
-name: "mac",
-version: "Catalina",
-},
-},
+    jsonDir: "cypress/cucumber-json",  // ** Path of .json file **//
+    reportPath: "./reports/cucumber-htmlreport.html",
+    metadata: {
+        browser: {
+        name: "Chrome",
+        version: "11",
+        },
+        device: "Hp Notebook 15",
+        platform: {
+        name: "Windows",
+        version: "10",
+        },
+    },
+    customData: {
+            title: "Run info",
+            data: [
+              { label: "Project", value: "Cypress project for The Souled Store" },
+              { label: "Release", value: "0.1" },
+              { label: "Cycle", value: "A1" },
+              { label: "Execution Start Time", value: "20th April 2023, 11:30 AM EST" },
+              { label: "Execution End Time", value: "22th April 2023, 11:50 PM EST" },
+            ],
+    },
 });
